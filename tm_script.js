79067c9311cde7cca5df81b801e9190fd2b107cc
cryeprecision <muench.rico@gmail.com>
2022-10-20 12:20:42 +0200

    Add `@run_at` to `tm_script.js`

--- tm_script.js
+++ tm_script.js
@@ -5,6 +5,7 @@
 // @description  Some scripts to improve UnKnoWnCheaTs
 // @author       Ideot
 // @match        https://www.unknowncheats.me/*
+// @run-at       document-idle
 // @grant        GM_getResourceText
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://raw.githubusercontent.com/cryeprecision/better_uc/feature/link-to-repo/unknowncheats.js
